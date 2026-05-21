1b4f4b814a670c11f3f46c5af8bd8288dcfd1e1d
Mixail-laboratory <spectrafa@gmail.com>
2026-05-21 08:16:48 +0000

    add metrics

---
+++ src/metric.cpp
@@ -0,0 +1,40 @@
+#include "metric.hpp"
+
+#include <unistd.h>
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "function.hpp"
+
+namespace analyzer::metric {
+void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) { metrics.push_back(std::move(metric)); }
+
+/**
+ * @brief Вычисляет все зарегистрированные метрики для заданной функции.
+ *
+ * Эта функция применяет каждый метрический объект из контейнера `metrics`
+ * к переданной функции `func` и собирает результаты в вектор.
+ */
+MetricResults MetricExtractor::Get(const function::Function &func) const {
+    MetricResults results;
+    results.reserve(metrics.size());
+    std::ranges::for_each(metrics,
+                          [&func, &results](const auto &metric) { results.push_back(metric->Calculate(func)); });
+    return results;
+}
+
+}  // namespace analyzer::metric
